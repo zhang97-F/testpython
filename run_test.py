533dd3dc05375fb76cffab6268f853647339a77c
zhang97-F <63094251+zhang97-F@users.noreply.github.com>
2020-08-20 11:59:25 +0800

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -12,8 +12,8 @@ testcases = load.discover('test_cares',pattern='test*.py',top_level_dir=None)
 # 把发现的用例放在容器中
 suite.addTests(testcases)
 # 设置测试报告的文档名字-----固定的名字reporter+当前系统时间+.html
-currenttime = time.strftime("%Y-%m-%d %H-%M-%S")
-filename = r'report/reporter'+currenttime+'.html'
+# currenttime = time.strftime("%Y-%m-%d %H-%M-%S")
+filename = r'report/reporter.html'
 # 生产测试报告  1、保证HTMLTestRunnerNew包要在commmon文件中  2、导包 import common.HTMLTestRunnerNew
 with open(filename,'wb+') as fp:
     runner = HT.HTMLTestRunner(stream=fp,title='学生管理系统接口测试报告',description='balalala',tester="zf")
